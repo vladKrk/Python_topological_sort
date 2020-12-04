144c09895bfe80e3ff28519c4a6a739e30fc61c1
Vlad <vladikzh@mail.ru>
2020-12-04 16:11:51 +0300

    Add 20 times loop

--- topological_sort.py
+++ topological_sort.py
@@ -34,21 +34,18 @@ def topological_sort(graph):
 
 def graph_generator(maxVert):
     graph = []
+    edges = 0
     for i in range(maxVert):
-        if i == 0:
-            pathes = 0
-        else:
-            pathes = random.randint(0, i)
         curVertList = []
+        pathes = random.randint(1, maxVert - 1)
         for j in range(pathes):
-            while(True):
-                if i == 1:
-                    new = 0
-                else:
-                    new = random.randint(0, i - 1)
-                if(new not in curVertList):
-                    break
-            curVertList.append(new)
+            if edges != maxVert:
+                while(True):
+                    new = random.randint(0, maxVert - 1)
+                    if(new != i and new not in curVertList):
+                        break
+                curVertList.append(new)
+                edges += 1
         graph.append(curVertList)
     return graph
 
@@ -70,21 +67,20 @@ def draw_graph(graph, num):
     return diGraph
 
 if __name__ == "__main__":
-    graph = graph_generator(random.randint(2, 10))
-    print("Исходный граф в виде списка смежности:", graph)
-    start_time = time.time()
-    answer = topological_sort(graph)
-    print("Линейный порядок вершин после топологической сортировки: ", answer)
-    print("Время работы: ", time.time() - start_time)
-    diGraph = draw_graph(graph, 221)
-    mapping = {}
-    for i, item in enumerate(answer):
-        mapping[item] = i
-    diGraph = nx.relabel_nodes(diGraph, mapping)
-    plt.subplot(222)
-    nx.draw_circular(diGraph,
-         node_color='blue',
-         node_size=450,
-         with_labels=True)
-    plt.show()
-    
+
+    for i in range(10):
+        length = 50000 * (i + 1)
+        print("Вершин | Рёбер: ", length , " | ", length)
+        for j in range(20):
+            graph = graph_generator(length)
+            times = []
+            start_time = time.time()
+            topological_sort(graph)
+            res_time = time.time() - start_time
+            times.append(res_time)
+        res = 0
+        for t in times:
+            res += t
+        res /= len(times)
+        print("Время среднее: ", res)
+
